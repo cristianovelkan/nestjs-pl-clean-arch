99113bee9b7af52ac81d08061ea960c0387fbd4f
Cristiano <cristianovelkan@gmail.com>
2025-05-12 07:57:31 -0300

    added class-validator to validate input data

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,4 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsPositive, IsNumber, MaxDate, IsDate } from 'class-validator'
+import { Transform } from 'class-transformer'
 export class CreateTransactionDto {
+  @IsPositive({ message: 'Transaction amount cannot be negative.' })
+  @IsNumber(
+    { allowNaN: false, allowInfinity: false },
+    { message: 'Transaction amount must be a valid number.' },
+  )
   amount: number
+
+  @Transform(({ value }) => new Date(value))
+  @IsDate({
+    message: 'Transaction timestamp must be a valid date.',
+  })
+  @MaxDate(new Date(), {
+    message: 'Transaction timestamp cannot be in the future.',
+  })
   timestamp: Date
 }
