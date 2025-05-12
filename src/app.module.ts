ba7697231f3b1782fc2a580ca74382065c1556c0
Cristiano <cristianovelkan@gmail.com>
2025-05-12 12:45:42 -0300

    Adds winston logging module
    Adds winston module for logging and configures it to log to files and console.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,49 @@ import { Module, MiddlewareConsumer } from '@nestjs/common'
 import { TransactionsModule } from './transactions/transactions.module'
 import { ThrottlerModule } from '@nestjs/throttler'
 import { JSONMiddleware } from './middlewares/json.middleware'
+import { WinstonModule } from 'nest-winston'
+import * as winston from 'winston'
 @Module({
   imports: [
+    WinstonModule.forRootAsync({
+      useFactory: () => ({
+        level: process.env.LOG_LEVEL || 'info',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.json(),
+        ),
+        defaultMeta: { service: 'transactions' },
+        transports: [
+          new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+          }),
+          new winston.transports.File({ filename: 'logs/combined.log' }),
+        ],
+        exceptionHandlers: [
+          new winston.transports.Console({
+            format: winston.format.combine(
+              winston.format.colorize(),
+              winston.format.simple(),
+            ),
+          }),
+          new winston.transports.File({
+            filename: 'logs/uncaught_exceptions.log',
+          }),
+        ],
+        rejectionHandlers: [
+          new winston.transports.Console({
+            format: winston.format.combine(
+              winston.format.colorize(),
+              winston.format.simple(),
+            ),
+          }),
+          new winston.transports.File({
+            filename: 'logs/unhandled_rejections.log',
+          }),
+        ],
+      }),
+    }),
     ThrottlerModule.forRoot({
       throttlers: [
         {
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('API Transactions')
